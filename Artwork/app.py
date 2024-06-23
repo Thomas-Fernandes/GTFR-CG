f7220c54e980eddd47333ed2925041fbc9d9e0e8
Quentin DI MEO <quentin.di-meo@epitech.eu>
2024-06-23 16:05:55 +0200

    adding cleaner code; fixing path issue for minia

--- Artwork/app.py
+++ Artwork/app.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, request, send_from_directory, session
+from flask import Flask, render_template, request, send_from_directory, session, Response
 from flask_session import Session
-from PIL import Image
-import os
-import uuid
-from functions import jaquette, miniature
+from os import makedirs, path
+from uuid import uuid4
+from functions import generateCoverArt, generateMinia
 from waitress import serve
 
 app = Flask(__name__)
-
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SESSION_FILE_DIR"] = 'Artwork/flask_session/'
@@ -16,43 +14,43 @@ Session(app)
 app.config['UPLOAD_FOLDER'] = 'Artwork/uploads/'
 app.config['PROCESSED_FOLDER'] = 'Artwork/processed/'
 
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-os.makedirs(app.config['PROCESSED_FOLDER'], exist_ok=True)
+makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+makedirs(app.config['PROCESSED_FOLDER'], exist_ok=True)
 
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def upload_file() -> str:
     if request.method == 'POST':
         file = request.files['file']
-        logo_position = request.form.get('logo-position', 'center')  # Valeur par défaut à 'center'
+        logo_position = request.form.get('logo-position', 'center')
         if file:
             if 'user_folder' not in session:
-                session['user_folder'] = str(uuid.uuid4())
+                session['user_folder'] = str(uuid4())
 
-            user_folder = session['user_folder']
-            user_upload_path = os.path.join(app.config['UPLOAD_FOLDER'], user_folder)
-            user_processed_path = os.path.join(app.config['PROCESSED_FOLDER'], user_folder)
-            os.makedirs(user_upload_path, exist_ok=True)
-            os.makedirs(user_processed_path, exist_ok=True)
+            user_folder: str = session['user_folder']
+            user_upload_path: str = path.join(app.config['UPLOAD_FOLDER'], user_folder)
+            user_processed_path: str = path.join(app.config['PROCESSED_FOLDER'], user_folder)
+            makedirs(user_upload_path, exist_ok=True)
+            makedirs(user_processed_path, exist_ok=True)
 
-            filepath = os.path.join(user_upload_path, file.filename)
+            filepath: str = path.join(user_upload_path, file.filename)
             file.save(filepath)
-            output_bg = os.path.join(user_processed_path, 'ProcessedArtwork.png')
-            output_minia = os.path.join(user_processed_path, 'minia.png')
+            output_bg = path.join(user_processed_path, 'ProcessedArtwork.png')
+            output_minia = path.join(user_processed_path, 'minia.png')
 
-            jaquette(filepath, output_bg)
-            miniature(output_bg, logo_position, output_minia)  # Utilisation de la position choisie
+            generateCoverArt(filepath, output_bg)
+            generateMinia(output_bg, logo_position, output_minia)
 
             return render_template('download.html', user_folder=user_folder, bg='ProcessedArtwork.png', minia='minia.png')
     return render_template('upload.html')
 
 
 @app.route('/download/<filename>')
-def download(filename):
+def download(filename: str) -> Response | tuple[str, int]:
     if 'user_folder' in session:
-        user_folder = session['user_folder']
-        directory = os.path.abspath(os.path.join(app.config['PROCESSED_FOLDER'], user_folder))
+        user_folder: str = session['user_folder']
+        directory: str = path.abspath(path.join(app.config['PROCESSED_FOLDER'], user_folder))
         return send_from_directory(directory, filename, as_attachment=True)
-    return "Session Expired or Invalid", 404
+    return ("Session Expired or Invalid", 404)
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8000)
--- Artwork/functions.py
+++ Artwork/functions.py
@@ -1,25 +1,30 @@
 
+from flask import session
 from PIL import Image, ImageFilter, ImageDraw
+from os import path, name as osName
 
-def jaquette(input, output):
-    image = Image.open(input)
-    
+MINIA_FOLDER = 'Artwork$Miniatures'
+
+def generateCoverArt(input_path: str, output_path: str) -> None:
+    image: Image.Image = Image.open(input_path)
+
     # Redimensionner l'image à 1920 de large tout en conservant les proportions
     base_width = 1920
     w_percent = (base_width / float(image.size[0]))
     h_size = int((float(image.size[1]) * float(w_percent)))
-    resized_image = image.resize((base_width, h_size), Image.Resampling.LANCZOS)
+    resized_image: Image.Image = image.resize((base_width, h_size), Image.Resampling.LANCZOS)
 
     # Recadrer l'image pour obtenir 1080 de hauteur (crop le reste)
-    top = (h_size - 1080) / 2
-    bottom = (h_size + 1080) / 2
-    cropped_image = resized_image.crop((0, top, 1920, bottom))
+    top = (h_size - 1080) // 2
+    bottom = (h_size + 1080) // 2
+    cropBox = (0, top, 1920, bottom)
+    cropped_image = resized_image.crop(cropBox)
 
     # flou gaussien sur l'image recadrée avec masque radial
-    blurred_image = cropped_image.filter(ImageFilter.GaussianBlur(radius=25))
+    blurred_image: Image.Image = cropped_image.filter(ImageFilter.GaussianBlur(radius=25))
 
     mask = Image.new("L", cropped_image.size, "black")
-    draw = ImageDraw.Draw(mask)
+    draw: ImageDraw.ImageDraw = ImageDraw.Draw(mask)
     max_dim = min(cropped_image.size) / 2
     center_x, center_y = cropped_image.size[0] // 2, cropped_image.size[1] // 2
 
@@ -29,21 +34,27 @@ def jaquette(input, output):
 
     final_blurred_image = Image.composite(cropped_image, blurred_image, mask)
 
-    center_image = image.resize((800, 800), Image.Resampling.LANCZOS)
+    center_image: Image.Image = image.resize((800, 800), Image.Resampling.LANCZOS)
     top_left_x = center_x - 400
     top_left_y = center_y - 400
     final_blurred_image.paste(center_image, (top_left_x, top_left_y))
 
-    final_blurred_image.save(output)
+    final_blurred_image.save(output_path)
 
-def miniature(background, logo, output):
-    background = Image.open(background)
-    overlay_file = f"Artwork/Miniatures/minia_{logo}.png"
-    overlay = Image.open(overlay_file)
+def generateMinia(bg_path: str, logo_path: str, output_path: str) -> None:
+    background: Image.Image = Image.open(bg_path)
+    user_folder: str = path.abspath(str(session['user_folder']))
+    if (osName != 'nt'): # Windows path type
+        user_folder = '/'.join(user_folder.split('/')[:-2]) # Remove the last folder and PID
+    else: # Unix path type
+        user_folder = '\\'.join(user_folder.split('\\')[:-2])
+    minia_folder = MINIA_FOLDER.replace('$', "/" if osName != 'nt' else '\\')
+    overlay_file = f"{user_folder}/{minia_folder}/minia_{logo_path}.png"
+    overlay: Image.Image = Image.open(overlay_file)
 
-    new_background = Image.new('RGBA', background.size)
+    new_background: Image.Image = Image.new('RGBA', background.size)
     new_background.paste(background, (0, 0))
     new_background.paste(overlay, mask=overlay)
 
     final_image = new_background.convert('RGB')
-    final_image.save(output)
+    final_image.save(output_path)
