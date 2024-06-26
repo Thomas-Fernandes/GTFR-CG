ab04edf9297a460d723fdce1adff06d6a8e8cad6
Thomas Fernandes <92884823+Thomas-Fernandes@users.noreply.github.com>
2024-06-26 23:42:57 +0200

    typo

--- Artwork/web_utils.py
+++ Artwork/web_utils.py
@@ -1,6 +1,6 @@
 from flask import jsonify
 
-def createJsonResponse(status_code: int, message: str = "") -> tuple:
+def createJsonResponse(status_code: int, message: str = "") -> tuple[str, int]:
     if (status_code == 200):
         status = 'success'
     elif (status_code in [400, 404, 500]):
