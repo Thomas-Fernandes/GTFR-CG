51519be5addc1a15819edf4c568e6f65d44b3869
Thomas Fernandes <92884823+Thomas-Fernandes@users.noreply.github.com>
2024-06-26 23:07:34 +0200

    fixing typo

--- Artwork/constants.py
+++ Artwork/constants.py
@@ -3,5 +3,5 @@ from enum import Enum
 class HttpStatus(Enum):
     OK = 200
     BAD_REQUEST = 400
-    INTERNAL_SERVER_ERROR = 500
     NOT_FOUND = 404
+    INTERNAL_SERVER_ERROR = 500
--- Artwork/web_utils.py
+++ Artwork/web_utils.py
@@ -2,6 +2,6 @@ from flask import jsonify
 
 def createJsonResponse(status_code: int, status: str, message: str = "") -> tuple[str, int]:
     response = {'status': status}
-    if message:
+    if (message):
         response['message'] = message
     return jsonify(response), status_code
