b070ca5b3b0ec897cc0fc47ed7573653494539a7
Thomas Fernandes <92884823+Thomas-Fernandes@users.noreply.github.com>
2024-06-26 22:45:32 +0200

    renaming var to const

--- Artwork/static/js/itunes_search.js
+++ Artwork/static/js/itunes_search.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
     $('#iTunesSearchForm').on('submit', function(event) {
         event.preventDefault();
 
-        var query = $('#query').val();
-        var country = $('#country').val();
-        var logoPosition = $('#logo-position-itunes').val();
+        const query = $('#query').val();
+        const country = $('#country').val();
+        const logoPosition = $('#logo-position-itunes').val();
 
         $.ajax({
             url: 'https://itunes.apple.com/search',
@@ -16,15 +16,15 @@ $(document).ready(function() {
             },
             dataType: 'jsonp',
             success: function(data) {
-                var resultsDiv = $('#results');
+                const resultsDiv = $('#results');
                 resultsDiv.empty();
                 if (data.results.length > 0) {
                     // first 5 results
                     data.results.slice(0, 5).forEach(function(result) {
                         // itunes max image size is 3000x3000
-                        var highResImageUrl = result.artworkUrl100.replace('100x100', '3000x3000');
-                        var img = $('<img>').attr('src', highResImageUrl).attr('alt', result.collectionName || result.trackName).addClass('result-image');
-                        var btn = $('<button>').text('Use this image').on('click', function() {
+                        const highResImageUrl = result.artworkUrl100.replace('100x100', '3000x3000');
+                        const img = $('<img>').attr('src', highResImageUrl).attr('alt', result.collectionName || result.trackName).addClass('result-image');
+                        const btn = $('<button>').text('Use this image').on('click', function() {
                             $.post('/use_itunes_image', { url: highResImageUrl, position: logoPosition }, function(response) {
                                 if (response.status === 'success') {
                                     window.location.href = '/process_itunes_image';
@@ -33,7 +33,7 @@ $(document).ready(function() {
                                 }
                             });
                         });
-                        var resultItem = $('<div>').addClass('result-item').append(img).append(btn);
+                        const resultItem = $('<div>').addClass('result-item').append(img).append(btn);
                         resultsDiv.append(resultItem);
                     });
                 } else {
