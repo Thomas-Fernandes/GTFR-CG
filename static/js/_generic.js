82ea14e5a300d5aa58d9a658aab2f4900467497f
Quentin DI MEO <q.dimeo@outlook.com>
2024-07-11 15:29:41 +0200

    better placing and style of spinner next to action buttons

--- static/js/_generic.js
+++ static/js/_generic.js
@@ -17,3 +17,36 @@ const ResponseStatus = Object.freeze({
     SUCCESS: "success",
     ERROR: "error"
 });
+
+const showSpinner = (name) => {
+    const button = document.getElementById(name);
+
+    // Create spinner container if it doesn't exist
+    if (!button.querySelector(".spinner-container")) {
+        const spinnerContainer = document.createElement("div");
+        spinnerContainer.classList.add("spinner-container");
+        spinnerContainer.id = "spinner-container" + "-" + name;
+
+        const spinner = document.createElement("span");
+        spinner.classList.add("spinner");
+
+        const favicon = document.createElement("img");
+        favicon.src = "/static/favicon.ico";
+        favicon.alt = "favicon";
+        spinner.appendChild(favicon);
+
+        spinnerContainer.appendChild(spinner);
+        button.appendChild(spinnerContainer);
+    }
+
+    // Show the spinner
+    const spinnerContainer = button.querySelector(".spinner-container");
+    spinnerContainer.style.display = "block";
+    spinnerContainer.style.alignSelf = "center";
+}
+const hideSpinner = (name) => {
+    const spinnerContainer = document.getElementById("spinner-container" + "-" + name);
+    if (spinnerContainer) {
+        spinnerContainer.style.display = "none";
+    }
+}
