924f055f81a43a00552919b6aaf37c6d11fa9da8
Quentin DI MEO <q.dimeo@outlook.com>
2024-07-21 01:32:29 +0200

    minor file adjustments, adding psi

--- installer.py
+++ installer.py
@@ -1,5 +1,5 @@
 from os import chdir, environ, name as osName, pathsep, system
-from subprocess import CalledProcessError, DEVNULL, CompletedProcess, Popen, run
+from subprocess import CalledProcessError, CompletedProcess, Popen, run
 from typing import Optional
 
 from src.logger import log, LogSeverity
@@ -9,32 +9,15 @@ def quitIfError(result: CompletedProcess[bytes]) -> None:
     """ Quits the installation if an error occurred.
     :param result: [CompletedProcess] The result of the subprocess.
     """
-    if result.returncode not in [0, 2316632107]:
+    if result.returncode not in [0, 2316632107]: # package installed, or already installed
         log.critical(f"Error while trying to install React: {result.stderr}")
         exit(1)
 
 @staticmethod
 def installNodePackages() -> None:
-    """ Installs the Node.js packages required by the front-end application.
+    """ Installs the Node packages required by the front-end application.
     """
     log.log("  Installing Node packages...")
-
-    def getNvmVersion() -> Optional[str]:
-        """ Checks if Nvm is installed.
-        :return: [str] The version of Nvm if it is installed, None otherwise.
-        """
-        try:
-            if osName == "nt": # Windows
-                run(["powershell", "-Command", "nvm v"], check=True, shell=True)
-                return ""
-            else: # Ubuntu
-                nvm_script = "$HOME/.nvm/nvm.sh"
-                result = run(f"source {nvm_script} && nvm --version",
-                    capture_output=True, text=True, check=True, shell=True, executable="/bin/bash"
-                )
-                return " " + result.stdout.strip()
-        except CalledProcessError:
-            return None
 
     def launchNodePackagesInstallation() -> None:
         chdir("GTFR-CG")
@@ -45,7 +28,32 @@ def installNodePackages() -> None:
         chdir("..")
         log.log("  Node packages installation complete.")
 
+    launchNodePackagesInstallation()
+
+@staticmethod
+def installNvm() -> None:
+    """ Installs Nvm, needed for Node
+    """
+
+    def getNvmVersion() -> Optional[str]:
+        """ Checks if Nvm is installed.
+        :return: [str] The version of Nvm if it is installed, None otherwise.
+        """
+        try:
+            if osName == "nt": # Windows
+                run(["powershell", "-Command", "nvm version"], check=True, shell=True)
+                return ""
+            else: # Ubuntu
+                nvm_script = "$HOME/.nvm/nvm.sh"
+                result = run(f"source {nvm_script} && nvm --version",
+                    capture_output=True, text=True, check=True, shell=True, executable="/bin/bash"
+                )
+                return " " + result.stdout.strip()
+        except CalledProcessError:
+            return None
+
     def launchNvmInstallation() -> None:
+        log.log("  Installing Nvm...")
         try:
             if osName == "nt":
                 chdir("GTFR-CG")
@@ -65,15 +73,16 @@ def installNodePackages() -> None:
     installedNvmVersion = getNvmVersion()
     if installedNvmVersion is not None:
         log.log(f"  Nvm{installedNvmVersion} is already installed.")
-    else:
-        launchNvmInstallation()
-    launchNodePackagesInstallation()
+        return
+    launchNvmInstallation()
 
 @staticmethod
 def installNode() -> None:
+    """ Installs Node.js, needed for React.
+    """
     def getNodeVersion() -> Optional[str]:
         """ Checks if Node.js is installed.
-        :return: [bool] True if Node.js is installed, False otherwise.
+        :return: [str] The version of Node.js if it is installed, None otherwise.
         """
         try:
             result = run(["node", "-v"], capture_output=True, text=True)
@@ -82,8 +91,6 @@ def installNode() -> None:
             return None
 
     def launchNodeJsInstallation() -> None:
-        """ Installs Node.js, needed for React.
-        """
         log.log("  Installing Node.js...")
 
         try:
@@ -99,19 +106,20 @@ def installNode() -> None:
                 quitIfError(run(["curl", "-fsSL", "https://deb.nodesource.com/setup_20.x", "-o", "/tmp/nodesource_setup.sh"], capture_output=True, check=True))
                 quitIfError(run(["sudo", "bash", "/tmp/nodesource_setup.sh"], capture_output=True, check=True))
                 quitIfError(run(["sudo", "apt-get", "install", "-y", "nodejs"], capture_output=True, check=True))
-
-            result = run(["node", "-v"], capture_output=True, text=True) # Verify installation
-            if result.returncode != 0:
-                log.error(f"  Error while verifying Node version: {result.stderr}")
         except CalledProcessError as e:
             log.critical(f"  Error while trying to install Node: {e}")
             exit(1)
-        log.log(f"  Node.js {result.stdout.strip()} installation complete.")
 
-    if osName == "nt": environ["Path"] += pathsep + r"C:\Program Files\nodejs"
+        result = run(["node", "-v"], capture_output=True, text=True) # Verify installation
+        if result.returncode != 0:
+            log.error(f"  Error while verifying Node version: {result.stderr}")
+        log.log(f"  Node {result.stdout.strip()} installation complete.")
+
+    if osName == "nt": # Windows
+        environ["Path"] += pathsep + r"C:\Program Files\nodejs" # Add Node.js to PATH
     installedNodeVersion = getNodeVersion()
     if installedNodeVersion is not None:
-        log.log(f"  Node.js {installedNodeVersion} is already installed.")
+        log.log(f"  Node {installedNodeVersion} is already installed.")
         return
     launchNodeJsInstallation()
 
@@ -120,6 +128,7 @@ def installReactReq() -> None:
     """
     log.log("Installing React requirements...")
     installNode()
+    installNvm()
     installNodePackages()
     log.log("React requirements installation complete.\n")
 
