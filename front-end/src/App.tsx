772d253d0dbd3d88ca979be9278d7e4c76aa54fa
Quentin DI MEO <q.dimeo@outlook.com>
2024-07-21 20:55:30 +0200

    adding router, css adjustments

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+// import Lyrics from "./pages/Lyrics";
+// import ProcessedImages from "./pages/ProcessedImages";
+// import ArtworkGeneration from "./pages/ArtworkGeneration";
+// import Redirect from "./pages/Redirect";
+import Home from "./pages/Home";
+import "./App.css";
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <body>
+    <Router>
+      <Routes>
+        {/* <Route path="/lyrics" element={<Lyrics />} />
+        <Route path="/processed-images" element={<ProcessedImages />} />
+        <Route path="/artwork-generation" element={<ArtworkGeneration />} />
+        <Route path="/redirect" element={<Redirect />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
+    </body>
   )
 }
 
