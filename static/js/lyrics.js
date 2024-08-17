119940e188590cf650e17e7c94ca7e213a72efe4
Thomas-Fernandes <92884823+Thomas-Fernandes@users.noreply.github.com>
2024-08-18 00:47:27 +0200

    lyrics lexer
    displaying lyrics in song sections ;
    hiding save button when search is not done ;
    saving lyrics in a list of dictionary : {"chorus" : "la la la"}

--- static/js/lyrics.js
+++ static/js/lyrics.js
@@ -3,8 +3,26 @@ const ErrorMsg = Object.freeze({
 });
 
 $(document).ready(function() {
-    if ($("textarea").val() === ErrorMsg.LYRICS_NOT_FOUND) {
+    const lyricsText = $("textarea").val();
+
+    if (lyricsText === ErrorMsg.LYRICS_NOT_FOUND) {
         sendToast(ErrorMsg.LYRICS_NOT_FOUND, ResponseStatus.WARN);
         $("textarea").val("");
+        $("#lyrics_save").hide();
+    } else if (lyricsText && lyricsText !== "") {
+        sendToast("Lyrics fetched successfully!", ResponseStatus.SUCCESS);
+        $("#lyrics_save").show();
+    } else {
+        $("#lyrics_save").hide();
     }
+
+    $('textarea').each(function() {
+        this.style.height = 'auto';
+        this.style.height = this.scrollHeight + 'px';
+
+        $(this).on('input', function() {
+            this.style.height = 'auto';
+            this.style.height = this.scrollHeight + 'px';
+        });
+    });
 });
