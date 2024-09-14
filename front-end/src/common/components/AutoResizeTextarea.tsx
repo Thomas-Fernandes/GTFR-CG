f9ec0f1209f2768d24ac0e14d2c7565a4b72423e
Thomas-Fernandes <92884823+Thomas-Fernandes@users.noreply.github.com>
2024-09-14 15:59:19 +0200

    Update AutoResizeTextarea.tsx

--- front-end/src/common/components/AutoResizeTextarea.tsx
+++ front-end/src/common/components/AutoResizeTextarea.tsx
@@ -2,29 +2,22 @@ import { forwardRef, useEffect, useRef } from "react";
 
 type AutoResizeTextareaProps = React.ComponentPropsWithoutRef<"textarea"> & { value: string };
 
-export const AutoResizeTextarea = forwardRef<HTMLTextAreaElement, AutoResizeTextareaProps>(
-  ({ value, ...props }, ref) => {
-    const internalRef = useRef<HTMLTextAreaElement>(null);
-    const combinedRef = ref || internalRef;
-
-    useEffect(() => {
-      const textarea = internalRef.current;
-
-      if (textarea) {
-        const resizeTextarea = () => {
-          textarea.style.height = "auto";
-          textarea.style.height = `${textarea.scrollHeight}px`;
-        };
-
-        resizeTextarea();
-        textarea.addEventListener("input", resizeTextarea);
-
-        return () => {
-          textarea.removeEventListener("input", resizeTextarea);
-        };
-      }
-    }, [value]);
-
-    return <textarea ref={combinedRef} value={value} {...props} />;
-  }
-);
+export const AutoResizeTextarea = forwardRef<HTMLTextAreaElement, AutoResizeTextareaProps>(({ value, ...props }, ref) => {
+  const internalRef = useRef<HTMLTextAreaElement>(null);
+  const combinedRef = ref || internalRef;
+  useEffect(() => {
+    const textarea = internalRef.current;
+    if (textarea) {
+      const resizeTextarea = () => {
+        textarea.style.height = "auto";
+        textarea.style.height = `${textarea.scrollHeight}px`;
+      };
+      resizeTextarea();
+      textarea.addEventListener("input", resizeTextarea);
+      return () => {
+        textarea.removeEventListener("input", resizeTextarea);
+      };
+    }
+  }, [value]);
+  return <textarea ref={combinedRef} value={value} {...props} />;
+});
