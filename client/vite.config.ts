1d94020afd30104e9c0e686505a07c466ae81408
Quentin DI MEO <q.dimeo@outlook.com>
2024-11-01 11:43:57 +0100

    [146] install @ syntax for pathing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    },
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
 })
