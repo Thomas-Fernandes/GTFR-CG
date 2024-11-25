e54c8271438260355d4268036e0c4142847b38cd
Quentin DI MEO <q.dimeo@outlook.com>
2024-11-25 01:04:18 +0100

    [156] implement dark mode (wip), background gif (wip)

--- client/src/components/DarkModeToggle/DarkModeToggle.tsx
+++ client/src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -6,7 +6,7 @@ const DarkModeToggle: React.FC = () => {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
 
   return (
-    <button onClick={toggleDarkMode} style={{width: "60%", alignSelf: "center"}}>
+    <button onClick={toggleDarkMode} style={{width: "40%", alignSelf: "center"}}>
       {isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
     </button>
   );
