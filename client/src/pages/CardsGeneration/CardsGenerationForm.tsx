a1e87776cbd1d9a8ef4aff40c0e0120ef8a8e121
Quentin DI MEO <q.dimeo@outlook.com>
2025-02-17 13:37:22 +0100

    [157] add formatMessage on lyrics and cardgen, add missing strings

--- client/src/pages/CardsGeneration/CardsGenerationForm.tsx
+++ client/src/pages/CardsGeneration/CardsGenerationForm.tsx
@@ -1,4 +1,3 @@
-
 import { ContentsGenerationMode } from "@/common/types";
 import ActionButton from "@/components/ActionButton/ActionButton";
 import Checkbox from "@/components/Checkbox/Checkbox";
@@ -24,73 +23,116 @@ const CardsGenerationForm: React.FC<CardsGenerationFormProps> = ({ setCardPaths,
     includeCenterArtwork: intl.formatMessage({ id: "pages.cardgen.selectors.centerArtwork" }),
     generateOutro: intl.formatMessage({ id: "pages.cardgen.selectors.outroImage" }),
     includeBackgroundImg: intl.formatMessage({ id: "pages.cardgen.selectors.bgImage" }),
-    submit: intl.formatMessage({ id: "pages.cardgen.submit" })
+    submit: intl.formatMessage({ id: "pages.cardgen.submit" }),
   };
 
   const {
-    cardMethod, cardMetaname, setCardMetaname, bgImg, colorPick, includeCenterArtwork,
-    generateOutro, includeBackgroundImg, cardBottomColor
+    cardMethod,
+    cardMetaname,
+    setCardMetaname,
+    bgImg,
+    colorPick,
+    includeCenterArtwork,
+    generateOutro,
+    includeBackgroundImg,
+    cardBottomColor,
   } = useCardsGenerationContext();
   const {
-    outroContributors, setOutroContributors, setBgImg, setColorPick, setIncludeCenterArtwork,
-    setGenerateOutro, setIncludeBackgroundImg, generationInProgress, setGenerationInProgress
+    outroContributors,
+    setOutroContributors,
+    setBgImg,
+    setColorPick,
+    setIncludeCenterArtwork,
+    setGenerateOutro,
+    setIncludeBackgroundImg,
+    generationInProgress,
+    setGenerationInProgress,
   } = useCardsGenerationFormContext();
 
   return (
-    <form id="settings"
-      onSubmit={(e) => handleGenerateCards(e,
-        {cardMetaname, outroContributors, bgImg, colorPick, includeCenterArtwork, generateOutro, includeBackgroundImg},
-        {generationInProgress, setGenerationInProgress, setCardPaths, setCards, setColorPick}
-      )}
+    <form
+      id="settings"
+      onSubmit={(e) =>
+        handleGenerateCards(
+          e,
+          {
+            cardMetaname,
+            outroContributors,
+            bgImg,
+            colorPick,
+            includeCenterArtwork,
+            generateOutro,
+            includeBackgroundImg,
+          },
+          { generationInProgress, setGenerationInProgress, setCardPaths, setCards, setColorPick }
+        )
+      }
     >
-      <label htmlFor="text-fields" className="hidden">{"Text fields"}</label>
+      <label htmlFor="text-fields" className="hidden">
+        {"Text fields"}
+      </label>
       <div id="text-fields" className="settings">
-        <input autoComplete="off"
-          type="text" name="metaname" placeholder={labels.metanamePlaceholder}
-          value={cardMetaname} onChange={(e) => setCardMetaname(e.target.value)}
+        <input
+          autoComplete="off"
+          type="text"
+          name="metaname"
+          placeholder={labels.metanamePlaceholder}
+          value={cardMetaname}
+          onChange={(e) => setCardMetaname(e.target.value)}
           className={!cardMetaname ? "empty" : ""}
         />
-        { generateOutro &&
-          <input autoComplete="off"
-            type="text" name="contributors" placeholder={labels.contributorsPlaceholder}
+        {generateOutro && (
+          <input
+            autoComplete="off"
+            type="text"
+            name="contributors"
+            placeholder={labels.contributorsPlaceholder}
             value={(outroContributors && "by: ") + outroContributors}
             onChange={(e) => setOutroContributors(e.target.value.replace("by: ", ""))}
-            className={`contributors ${(!outroContributors ? "empty" : "")}`}
+            className={`contributors ${!outroContributors ? "empty" : ""}`}
           />
-        }
+        )}
       </div>
 
-      <label htmlFor="enforcers" className="hidden">{"Enforcers"}</label>
+      <label htmlFor="enforcers" className="hidden">
+        {"Enforcers"}
+      </label>
       <div id="enforcers" className="settings">
         <FileUploader
-          id="background-image" label={labels.fileUploadLabel} caption={labels.fileUploadButton}
-          accept="image/*" setter={setBgImg}
+          id="background-image"
+          label={labels.fileUploadLabel}
+          caption={labels.fileUploadButton}
+          accept="image/*"
+          setter={setBgImg}
         />
-        <ColorPicker
-          id="bottom-bar" label={labels.colorPickerLabel}
-          latest={cardBottomColor} setter={setColorPick}
-        />
+        <ColorPicker id="bottom-bar" label={labels.colorPickerLabel} latest={cardBottomColor} setter={setColorPick} />
       </div>
 
-      <label htmlFor="selectors" className="hidden">{"Selectors"}</label>
+      <label htmlFor="selectors" className="hidden">
+        {"Selectors"}
+      </label>
       <div id="selectors" className="settings">
-        { bgImg &&
-          <Checkbox id="include_center_artwork"
+        {bgImg && (
+          <Checkbox
+            id="include_center_artwork"
             size={24}
             checked={includeCenterArtwork}
             onChange={() => setIncludeCenterArtwork(!includeCenterArtwork)}
             label={labels.includeCenterArtwork}
           />
-        }
+        )}
         <div onClick={() => cardMethod === ContentsGenerationMode.Manual && handleUnauthorizedCheckbox()}>
-          <Checkbox id="generate_outro" disabled={cardMethod === ContentsGenerationMode.Manual}
+          <Checkbox
+            id="generate_outro"
+            disabled={cardMethod === ContentsGenerationMode.Manual}
             size={24}
             checked={cardMethod === ContentsGenerationMode.Auto && generateOutro}
             onChange={() => cardMethod === ContentsGenerationMode.Auto && setGenerateOutro(!generateOutro)}
             label={labels.generateOutro}
           />
         </div>
-        <Checkbox id="include_background"
+        <Checkbox
+          id="include_background"
           size={24}
           checked={includeBackgroundImg}
           onChange={() => setIncludeBackgroundImg(!includeBackgroundImg)}
@@ -102,7 +144,7 @@ const CardsGenerationForm: React.FC<CardsGenerationFormProps> = ({ setCardPaths,
         <ActionButton type="submit" label={labels.submit} className="spaced" />
       </div>
     </form>
-  )
+  );
 };
 
 export default CardsGenerationForm;
--- client/src/pages/Lyrics/constants.ts
+++ client/src/pages/Lyrics/constants.ts
@@ -2,11 +2,11 @@ export const CONTENT_THRESHOLDS = {
   DISPLAY_LIMIT: 6,
   LINES: {
     ERROR: 5,
-    WARNING: 4
+    WARNING: 4,
   },
   LINE_PX_LENGTH: {
     ERROR: 355, // goes beyond the card
-    WARNING: 342 // goes beyond the margin
+    WARNING: 342, // goes beyond the margin
   },
 };
 
